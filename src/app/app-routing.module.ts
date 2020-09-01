22fae09e8b30bb2c5188d39e42d601bc0dc37984
mbobrowska93 <m.bobrowska93@wp.pl>
2020-09-01 18:54:24 +0200

    routing for many components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component';
+import { ThreeComponent } from './three/three.component';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'one', component: OneComponent},
+  {path: 'two', component: TwoComponent},
+  {path: 'three', component: ThreeComponent},
+  { path: '',   redirectTo: '/one', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+// export const myComponents=[OneComponent, TwoComponent, ThreeComponent]
+// wtedy w app module zamiast importowac komponenty importuje myComponents
+
